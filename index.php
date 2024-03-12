6f36af50b85e961b3de27eb7b290621bbea25ab8
Gabriel Gravena Barros <109182123+jotafreitas@users.noreply.github.com>
2024-03-12 18:33:33 -0300

    dia 1 php

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>EXERCÍCIOS 12/03/2024</h1>
+
+<?php
+
+// $num1 = 101.7589;
+// $num2 = 202.475;
+// $soma = $num1+$num2;
+
+// echo "a some de $num1+$num2=$soma";
+// echo "<br><br><br><br>";
+// echo "E AGORA EM ",number_format($soma,2,",",".");
+// echo "<br><br><br><br>";
+// $nota=[];
+// $nota[1]=7;
+// $nota[2]=6;
+// $nota[3]=7;
+// $nota[4]=6;
+// echo "A nota 1 é = $nota[1] <br>";
+// $media = ($nota[1]+$nota[2]+$nota[3]+$nota[4])/4;
+// echo "A media é ".$media."<br>";
+// $situacao = $media>=6?"Aprovado":"Reprovado";
+// echo "Situação ".$situacao."<br>";
+
+// echo "<br><br><br><br>";
+
+// FIM DO PRIMEIRO EXERCÍCIO
+
+//COMEÇO DO SEGUNDO
+
+// for ($i=1;$i<=10;$i++){
+//     echo ("<br>$i");
+// }
+
+// $n1 = 10;
+// $n2 = 20;
+// $soma = $n1+$n2;
+// $mult = $soma*$n1;
+
+// echo "A SOMA DE $n1 + $n2 é de $soma<br>A múltiplicação do $n1 com a $soma é de $mult"
+
+//terceiro exercício:
+// $val1 = 10;
+// $val2 = 10;
+// $val3 = 10;
+
+// $media = ($val1+$val2+$val3)/3;
+
+// echo $media>=7?"aprovado":"reprovado";
+// if ($media==10){
+//     echo " com distinção";
+// };
+
+//quarto exercício e quinto exercício
+
+// $val1 = 200;
+
+// $quinze = $val1*0.15;
+// $cinco = $val1*0.05;
+// $cinquenta = $val1*0.50;
+
+
+// echo "5% de $val1 é $cinco";
+// echo "<br>";
+// echo "15% de $val1 é $quinze";
+// echo "<br>";
+// echo "50% de $val1 é $cinquenta";
+
+//sexto exercício
+// $quadrado1 = 5;
+// $quadrado2 = 15;
+
+// $soma1 = $quadrado1**2;
+// $soma2 = $quadrado2**2;
+
+// $total = $soma1+$soma2;
+
+// echo ("<a class='jorge'> A soma dos quadrados é de $total </a>");
+
+//sétimo exercício
+// $mps = 15;
+// $kmh = $mps*3.6;
+
+// echo("<a class='jorge'> A velocidade de $mps m/s equivale a $kmh km/h  </a>")
+
+//oitavo exercício
+// $altura = 1.75;
+// $peso = 60;
+
+// $imc = ($peso/($altura*$altura));
+
+// echo "$imc kg/m2";
+
+?>
+
+
+<br>
+<h4>FLOR DO CRISTIAN:</h4>
+<img style="height:50px; width: 50px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO7BomJiIFfYiYRat8vpjV8O5_-fKQyuE80w&usqp=CAU">
+
+</body>
+</html>
